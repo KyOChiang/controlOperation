f1831afb2843d3768bc00e2a3c28b666a10e24b7
KyO <yong931231@hotmail.com>
2014-04-07 20:26:23 +0800

    Add test for CALL to test it will go to expected address or not. It will, it start to push the next 2 instruction address to TOS. Then, the STKPTR will increasing and it will go to the expected address.

--- test/test_CALL.c
+++ test/test_CALL.c
@@ -69,3 +69,55 @@ void test_CALL_should_throw_exception_if_range_between_absoluteAddress_and_opera
 	
 	TEST_ASSERT_EQUAL(0xAA,code.absoluteAddress);
 }
+
+void test_CALL_should_go_to_0x5204_and_0xAC_will_push_to_TOS_and_STKPTR_increment_by1(){
+	CEXCEPTION_T operandERR;
+	
+	Bytecode code = {.instruction = {.mnemonic = CALL, .name = "CALL"},
+					 .operand1 = 0x5204,
+					 .operand2 = -1,
+					 .operand3 = -1,
+					 .absoluteAddress = 0xAA
+					 };	
+	//Next two address from 0xAA - 0xAC
+	FSR[STKPTR] = 0;
+	FSR[TOSU] = 0x00;
+	FSR[TOSH] = 0x00;
+	FSR[TOSL] = 0x00;
+	Try{
+		call(&code);
+	}Catch(operandERR){
+		TEST_ASSERT_EQUAL(ERR_INVALID_OPERAND1,operandERR);
+	}
+	TEST_ASSERT_EQUAL_HEX8(0x5204,code.absoluteAddress);
+	TEST_ASSERT_EQUAL_HEX8(0x00,FSR[TOSU]);
+	TEST_ASSERT_EQUAL_HEX8(0x00,FSR[TOSH]);
+	TEST_ASSERT_EQUAL_HEX8(0xAC,FSR[TOSL]);
+	TEST_ASSERT_EQUAL(1,FSR[STKPTR]);
+}
+
+void test_CALL_should_go_to_0xAC_and_0x5026_will_push_to_TOS_and_STKPTR_increment_by1(){
+	CEXCEPTION_T operandERR;
+	
+	Bytecode code = {.instruction = {.mnemonic = CALL, .name = "CALL"},
+					 .operand1 = 0xAC,
+					 .operand2 = -1,
+					 .operand3 = -1,
+					 .absoluteAddress = 0x5024
+					 };	
+	//Next two address from 0x5024 - 0x5026
+	FSR[STKPTR] = 1;
+	FSR[TOSU] = 0x00;
+	FSR[TOSH] = 0x00;
+	FSR[TOSL] = 0xAC;
+	Try{
+		call(&code);
+	}Catch(operandERR){
+		TEST_ASSERT_EQUAL(ERR_INVALID_OPERAND1,operandERR);
+	}
+	TEST_ASSERT_EQUAL_HEX8(0xAC,code.absoluteAddress);
+	TEST_ASSERT_EQUAL_HEX8(0x00,FSR[TOSU]);
+	TEST_ASSERT_EQUAL_HEX8(0x50,FSR[TOSH]);
+	TEST_ASSERT_EQUAL_HEX8(0x26,FSR[TOSL]);
+	TEST_ASSERT_EQUAL(2,FSR[STKPTR]);
+}
